f5d5b6bc8bba41fd6f163e5cee0db53a73418bda
Roan OSullivan <roanosullivan@sosuke.729x.com>
2014-06-26 13:35:12 -0400

    Another KendoGrid enhancement, this time to save dataSource state (e.g. sort, filter, group, paging) before re-rendering. This preserves user changes across updates.

--- js/controls/KendoGrid.js
+++ js/controls/KendoGrid.js
@@ -18,6 +18,13 @@ define([
             };
         },
 
+        getInitialState: function (){
+            return {
+                widget: null,
+                widgetState: null
+            };
+        },
+
         render: function () {
             console.assert(this.props.dataSource);
             console.assert(this.props.columns);
@@ -40,21 +47,66 @@ define([
             // trigger data binding AFTER parent has reference to widget to allow event handlers
             // to first get reference to the grid widget
             gridWidget.dataSource.read();
+            this.setState({widget: gridWidget});
         },
 
-        componentDidUpdate: function (prevProps, prevState) {
-            var $el = $(this.getDOMNode());
-            var kendoWidget = $el.data('kendoGrid');
+        componentWillUpdate: function(nextProps, nextState){
+            // need to capture current widget state (and reapply in componentDidUpdate)
+            nextState['widgetState'] = this.getWidgetState();
+        },
 
+        componentDidUpdate: function(prevProps, prevState) {
+            var widget = this.state.widget;
+
+            // Reset datasource since it might have changed
             if (this.props.dataSource instanceof Array) {
                 // This better be a datasource that was originally built from inline data.
                 // I don't know how to detect this to verify it.
-                kendoWidget.dataSource.data(this.props.dataSource);
+                widget.dataSource.data(this.props.dataSource);
             }
             else if (prevProps.dataSource !== this.props.dataSource) {
-                kendoWidget.setDataSource(this.props.dataSource);
+                widget.setDataSource(this.props.dataSource);
+            }
+            // Since we are not autobinding, need to explicitly re-read data source, ..
+            widget.dataSource.read();
+            // Finally, need to re-apply last user-made changes widget state (e.g. sorting, grouping, filtering, etc.)
+            // that was captured in componentWillUpdate
+            this.setWidgetState(this.state.widgetState);
+
+        },
+
+        /**
+         * Based on http://www.telerik.com/forums/saving-grid-state, which refers to
+         * this fiddle: http://jsbin.com/erefug/1/edit
+         * @returns {{page: *, pageSize: *, sort: *, filter: *, group: *}}
+         */
+        getWidgetState: function () {
+            var widget = this.state.widget;
+            if(!widget){
+                return null;
+            }
+            // todo: also capture group collapsed/expanded state (see http://demos.telerik.com/kendo-ui/grid/api)
+            return {
+                page: widget.dataSource.page(),
+                pageSize: widget.dataSource.pageSize(),
+                sort: widget.dataSource.sort(),
+                filter: widget.dataSource.filter(),
+                group: widget.dataSource.group()
+            };
+        },
+
+        setWidgetState: function(state){
+            var widget = this.state.widget;
+            if(widget && state){
+                widget.dataSource.filter(state.filter);
+                widget.dataSource.sort(state.sort);
+                widget.dataSource.group(state.group);
+                widget.dataSource.pageSize(state.pageSize);
+                widget.dataSource.page(state.page);
             }
         }
+
+
     });
 
     return KendoGrid;
